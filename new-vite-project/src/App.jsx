21f10bedbbde7a1c83bbc2f65aa0803a4b3c4b37
rafia-khan <khanrafia9218@gmail.com>
2024-06-10 18:53:28 +0600

    first commit

---
+++ new-vite-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import React, { Fragment } from 'react';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+ <Fragment>
+   <Header/>
+   <Footer/>
+   
+   </Fragment>
+  );
+};
+
+export default App;
